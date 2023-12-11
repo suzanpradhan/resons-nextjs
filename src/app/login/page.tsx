280b51fb1bf18e60b7d78753db587cceb8e5f428
Avishek Dahal <dahalavishek2022@gmail.com>
2023-12-11 16:20:56 +0545

    fix: login routing issue

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,8 @@
 'use client';
-import { useAppDispatch, useAppSelector } from '@/core/redux/clientStore';
-import { RootState } from '@/core/redux/store';
+import { useAppDispatch } from '@/core/redux/clientStore';
 import Button from '@/core/ui/components/Button';
 import TextField from '@/core/ui/components/TextField';
 import { LoginRequestType, loginFormSchema } from '@/modules/login/loginType';
-import socialApi from '@/modules/social/socialAPI';
 import { useFormik } from 'formik';
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
@@ -21,127 +19,29 @@ export default function SignIn() {
   const dispatch = useAppDispatch();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | undefined>(undefined);
-  // const [authenticateChecked, setAuthenticateChecked] = useState(false);
-  // const [isAllow, setIsAllow] = useState(() => {
-  //   // Check if the authentication cookie is present
-  //   const isAuthenticated = Cookies.get('authenticated') === 'true';
-  //   return isAuthenticated;
-  // });
-  // const [errors, setErrors] = useState({
-  //   email: '',
-  //   password: '',
-  // });
-
-  // const [password, setPassword] = useState('');
-  // const defaultPasss = process.env.NEXT_PUBLIC_DEFAULT_PASS
-  //   ? process.env.NEXT_PUBLIC_DEFAULT_PASS
-  //   : '';
-
-  // const handlePasswordCheck = () => {
-  //   // Replace 'your_expected_password' with the actual expected password
-  //   if (password === defaultPasss) {
-  //     // Set a cookie to indicate the user is authenticated
-  //     Cookies.set('authenticated', 'true', { expires: 365 }); // Cookie expires in 1 year
-  //     navigator.push('/');
-  //   } else {
-  //     alert('Worng password!');
-  //   }
-  // };
-
-  // const handleChange = (event: any) => {
-  //   formik.setFieldValue(event.target.name, event.target.value);
-  // };
-
-  // const loginFormSchema = z.object({
-  //   email: z.string().email(),
-  //   password: z.string(),
-  // });
-
-  // type LoginRequestType = z.infer<typeof loginFormSchema>;
 
   const onSubmit = async (values: LoginRequestType) => {
+    console.log(callbackUrl);
     setIsLoading(true);
     await signIn('credentials', {
       email: values.email,
       password: values.password,
       platform: 'mobile',
       redirect: false,
-    })
-      .then((response) => {
-        if (response?.ok) {
-          console.log(response);
-          navigator.push(callbackUrl ? callbackUrl : '/');
+    }).then((response) => {
+      if (response?.ok) {
+        if (callbackUrl) {
+          window.location.href = callbackUrl;
+          if (callbackUrl.includes('#')) window.location.reload();
         } else {
-          // console.log(response);
-          // alert(JSON.stringify(response));
-          setError(response?.error!);
+          navigator.refresh();
         }
-      })
-      .catch((errorResponse) => {
-        // alert(JSON.stringify(errorResponse));
-      });
+      } else {
+        setError(response?.error!);
+      }
+    });
     setIsLoading(false);
   };
-
-  // const onSubmit = async (values: LoginRequestType) => {
-  //   setIsLoading(true);
-
-  //   if (!values.email || values.email === '') {
-  //     setErrors((prevErrors) => ({
-  //       ...prevErrors,
-  //       email: 'Email is required.',
-  //     }));
-  //     setIsLoading(false);
-  //     return false;
-  //   } else {
-  //     const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-  //     const isValidEmail = emailPattern.test(values.email);
-
-  //     if (isValidEmail) {
-  //       setErrors((prevErrors) => ({ ...prevErrors, email: '' }));
-  //     } else {
-  //       setErrors((prevErrors) => ({
-  //         ...prevErrors,
-  //         email: 'Invalid email format.',
-  //       }));
-  //     }
-  //   }
-
-  //   if (!values.password || values.password === '') {
-  //     setErrors((prevErrors) => ({
-  //       ...prevErrors,
-  //       password: 'Password is required.',
-  //     }));
-  //     setIsLoading(false);
-  //     return false;
-  //   }
-
-  //   const csrfToken = await fetch(`${apiPaths.rootPath}${apiPaths.csrfPath}`);
-
-  //   if (csrfToken?.status === 204) {
-  //     const response = await dispatch(
-  //       loginAPI.endpoints.login.initiate(values)
-  //     );
-
-  //     if (response && 'error' in response) {
-  //       navigator.push('/login');
-  //     } else {
-  //       const { optional, data } = response.data;
-  //       console.log(response);
-  //       // Cookies.set('user_id', data.id);
-  //       // Cookies.set('user_name', data.name);
-  //       // Cookies.set('email', data.email);
-  //       Cookies.set('token', optional.token, { expires: 365 });
-
-  //       navigator.push('/');
-  //     }
-  //   } else {
-  //     setIsLoading(false);
-  //     toast.error('Failed to Login');
-  //   }
-
-  //   setIsLoading(false);
-  // };
 
   const formik = useFormik({
     initialValues: {
@@ -151,38 +51,6 @@ export default function SignIn() {
     validate: toFormikValidate(loginFormSchema),
     onSubmit,
   });
-
-  const handleSocialUrl = async () => {
-    setIsLoading(true); // Set isLoading to true before making the API request
-    await dispatch(socialApi.endpoints.getSocialUrl.initiate(undefined));
-    setIsLoading(false); // Set isLoading to false after the request is completed
-  };
-
-  const socialUrl = useAppSelector(
-    (state: RootState) => state.baseApi.queries?.data as any
-  );
-
-  // useEffect(() => {
-  //   // Check if the authentication cookie is present
-  //   const isAuthenticated = Cookies.get('authenticated') === 'true';
-  //   const isToken = Cookies.get('token');
-
-  //   if (isAuthenticated) {
-  //     if (isToken) {
-  //       navigator.push('/');
-  //       return;
-  //     }
-  //     setAuthenticateChecked(true);
-  //     setIsAllow(true);
-  //   } else {
-  //     setAuthenticateChecked(true);
-  //     setIsAllow(false);
-  //   }
-  // }, []);
-
-  // if (!authenticateChecked) {
-  //   return null;
-  // }
 
   return (
     <>
@@ -200,11 +68,7 @@ export default function SignIn() {
             </div>
             <h1 className="font-bold text-gray-700">Resons</h1>
           </div>
-          {/* Login Options */}
           <div className="flex flex-col items-center justify-center gap-4">
-            {/* <button className="w-full max-w-sm py-2 px-3 bg-[#3578e5] border-[#3578e5] border text-white text-base font-medium rounded-full">
-            Continue with Facebook
-          </button> */}
             <GoogleSignInButton />
           </div>
 
@@ -224,8 +88,6 @@ export default function SignIn() {
                 placeholder="Your Email Address"
                 id="email"
                 type="email"
-                // name="email"
-                // onChange={handleChange}
                 {...formik.getFieldProps('email')}
               />
               {!!formik.errors.email && (
@@ -238,8 +100,6 @@ export default function SignIn() {
                 placeholder="Your Password"
                 id="password"
                 type="password"
-                // name="password"
-                // onChange={handleChange}
                 {...formik.getFieldProps('password')}
               />
               {!!formik.errors.password && (
