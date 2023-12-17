9af91b952e05de2b7c07c24fbbac83d4a9b614e6
Avishek Dahal <dahalavishek2022@gmail.com>
2023-12-17 14:20:39 +0545

    fix: language post list and topic list scroll type error

--- src/app/(home)/explore/(components)/LanguagePostList.tsx
+++ src/app/(home)/explore/(components)/LanguagePostList.tsx
@@ -10,52 +10,52 @@ const LanguagePostList = ({ title }: LanggatePostListType) => {
       groupTitle: title,
       slides: [
         {
-          img_url:
+          image:
             'https://marketplace.canva.com/EAEqlr422aw/1/0/1600w/canva-falling-modern-aesthetic-music-album-cover-KsRCFSNg4XA.jpg',
           title: 'English',
         },
         {
-          img_url:
+          image:
             'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
           title: 'Nepali',
         },
         {
-          img_url:
+          image:
             'https://images.template.net/wp-content/uploads/2022/06/Thriller-Album-Cover.jpg',
           title: 'Hindi',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Chinese',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Korean',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Spanish',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Telugu',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Mongolian',
         },
         {
-          img_url:
+          image:
             'https://marketplace.canva.com/EAEqlr422aw/1/0/1600w/canva-falling-modern-aesthetic-music-album-cover-KsRCFSNg4XA.jpg',
           title: 'Arabian',
         },
         {
-          img_url:
+          image:
             'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
           title: 'African',
         },
--- src/app/(home)/explore/(components)/TopicListScroll.tsx
+++ src/app/(home)/explore/(components)/TopicListScroll.tsx
@@ -10,52 +10,52 @@ const TopicListScroll = ({ title }: { title: string }) => {
       groupTitle: title,
       slides: [
         {
-          img_url:
+          image:
             'https://marketplace.canva.com/EAEqlr422aw/1/0/1600w/canva-falling-modern-aesthetic-music-album-cover-KsRCFSNg4XA.jpg',
           title: 'Slide 1',
         },
         {
-          img_url:
+          image:
             'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
           title: 'Slide 2',
         },
         {
-          img_url:
+          image:
             'https://images.template.net/wp-content/uploads/2022/06/Thriller-Album-Cover.jpg',
           title: 'Slide 3',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Slide 4',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Slide 5',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Slide 6',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Slide 7',
         },
         {
-          img_url:
+          image:
             'https://cdn.create.vista.com/downloads/7cac6f71-10bc-4d55-9da8-10a67cb46cce_450.jpeg',
           title: 'Slide 8',
         },
         {
-          img_url:
+          image:
             'https://marketplace.canva.com/EAEqlr422aw/1/0/1600w/canva-falling-modern-aesthetic-music-album-cover-KsRCFSNg4XA.jpg',
           title: 'Slide 9',
         },
         {
-          img_url:
+          image:
             'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
           title: 'Slide 10',
         },
