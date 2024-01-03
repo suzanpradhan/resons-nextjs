91c1b8040e0c6726784e342e6c12a862a0b747c5
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2024-01-03 11:10:45 +0545

    fix: post expiration ui

--- src/app/(home)/post/(components)/AddExpiration.tsx
+++ src/app/(home)/post/(components)/AddExpiration.tsx
@@ -1,25 +1,59 @@
-import '@/app/calendar.css';
+import classNames from 'classnames';
 import { useState } from 'react';
-import { Value } from 'react-calendar/dist/cjs/shared/types';
 import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const AddExpiration = () => {
-  const [date, setDate] = useState<Date | string | undefined>(new Date());
-  const handleChange = (e: Value) => {
-    const dateString = e?.toLocaleString();
-    setDate(dateString);
-  };
-
   const [startDate, setStartDate] = useState(new Date());
+  const [currentValue, setValue] = useState(0);
+
+  const expirationTypes = ['Never', 'Day', 'Week', 'Custom'];
 
   return (
-    <div className="w-full flex justify-center pt-5 px-4 bg-white flex-1 overflow-y-scroll">
-      {/* <Calendar onChange={(e) => handleChange(e)} value={date} /> */}
-      <DatePicker
-        selected={startDate}
-        onChange={(date: Date) => setStartDate(date)}
-      />
-    </div>
+    <>
+      <div className="px-4 py-2 border-b-2 bg-white">Post Audience</div>
+      <div className="bg-white flex-1 overflow-y-scroll pb-14">
+        {expirationTypes.map((item, index) => (
+          <div key={index}>
+            <label
+              className={classNames(
+                `mb-1 w-full flex items-center p-4 font-normal text-base`,
+                Number(currentValue) === index ? 'bg-red-500' : 'bg-whiteShade',
+                Number(currentValue) === index ? 'text-white' : 'text-dark-500'
+              )}
+              htmlFor={'privacy_code_' + item}
+            >
+              <span className="ml-1">{item}</span>
+              <div className="ml-4">
+                {item == 'Custom' ? (
+                  <DatePicker
+                    minDate={startDate}
+                    popperPlacement="top"
+                    className={classNames(
+                      ' rounded-md text-dark-500 ',
+                      currentValue == index ? 'bg-red-200' : 'bg-white'
+                    )}
+                    showIcon
+                    selected={startDate}
+                    onChange={(date: Date) => setStartDate(date)}
+                  />
+                ) : (
+                  <></>
+                )}
+              </div>
+            </label>
+            <input
+              hidden
+              id={'privacy_code_' + item}
+              type="radio"
+              name="privacy_code"
+              value={index}
+              onChange={() => setValue(index)}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
